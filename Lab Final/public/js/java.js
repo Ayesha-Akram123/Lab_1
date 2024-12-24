65e684973d3289bc1670514e551b52376653cdc1
Ayesha-Akram123 <ayeshaakramcheema123@gmail.com>
2024-12-24 16:34:55 +0500

    hhh

--- Lab Final/public/js/java.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("profile-image").addEventListener("mouseover", function() {
-    document.getElementById("intro").style.display = "block";
-});
-
-document.getElementById("profile-image").addEventListener("mouseout", function() {
-    document.getElementById("intro").style.display = "none";
-});
